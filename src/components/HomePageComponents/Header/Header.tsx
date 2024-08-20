61df0eaa5306aec751218ea9852432000cf9f83a
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-08-20 12:39:58 +0300

    Header Improvements | Header

--- src/components/HomePageComponents/Header/Header.tsx
+++ src/components/HomePageComponents/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const t = useTranslations("Header")
 
   return (
-    <div className="relative lg:h-[calc(100dvh-4.75rem)] max-lg:pb-[2.667rem] bg-[url('/images/header_bg.png')] bg-bottom bg-cover lg:px-[12rem] px-[1rem] lg:pt-[30vh] pt-[13.75rem]">
+    <div className="relative lg:h-[calc(100dvh-9.25rem)] max-lg:pb-[2.667rem] bg-[url('/images/header_bg.png')] bg-center bg-cover lg:px-[12rem] px-[1rem] lg:pt-[30vh] pt-[13.75rem]">
         <h1 className='md:text-[6rem] text-[2.70rem] font-bold font-montserrat text-white uppercase'><span className='text-red'>{t('ticketsWord1')}</span> { t('ticketsWord2') }</h1>
         <div className='md:text-[2rem] text-[1.35rem] font-bold text-white uppercase flex md:gap-[1rem] gap-[0.5rem]  items-center'>
           <h3>{t('country1')}</h3>
