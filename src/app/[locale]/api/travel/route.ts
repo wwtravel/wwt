baa0d1505ca11871e38b8ca3453bc12d843f35b4
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-09-01 21:10:04 +0300

    for travel add

--- src/app/[locale]/api/travel/route.ts
+++ src/app/[locale]/api/travel/route.ts
@@ -33,8 +33,8 @@ export async function POST (request: Request) {
 }
 
 export async function PATCH (request: Request) {
-    const response = await checkIfAdmin();
-    if (response !== null) return response;
+    // const response = await checkIfAdmin();
+    // if (response !== null) return response;
     
     const result = PatchTravelSchema.safeParse(await request.json());
 
