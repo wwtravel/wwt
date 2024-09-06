314a301ed0384cca2020bbf665c7e3a49d566505
VadimOMEGA <vadimivanov.0320@gmail.com>
2024-09-06 23:05:03 +0300

    Not Found Page Ready | Not Found Page

--- src/components/NotFoundPageComponents/NotFoundContent.tsx
+++ src/components/NotFoundPageComponents/NotFoundContent.tsx
@@ -24,23 +24,23 @@ const NotFoundContent = () => {
             return prev - 1;
           });
         }, 1000);
-        
+
         return () => clearInterval(intervalId);
       }, [router]);
 
   return (
-    <div className='w-full pt-[14rem] px-[1rem]'>
+    <div className='w-full pt-[12rem] px-[1rem]'>
       <div className='max-w-[84.25rem] w-full mx-auto h-[40.875rem] bg-[url(/images/dotted-map-bg.png)] bg-cover bg-center flex justify-center'>
       <div className='mt-[3.5rem]'>
-        <div className='flex gap-[0.5rem] items-center h-fit'>
-            <p className='font-montserrat text-dark-gray font-bold text-[20rem] leading-[0.7]'>4</p>
-            <img src="/images/wheel.png" alt="wheel" draggable={false} className='size-[16rem]'/>
-            <p className='font-montserrat text-dark-gray font-bold text-[20rem] leading-[0.7]'>4</p>
+        <div className='flex gap-[0.5rem] items-center h-fit justify-center'>
+            <p className='font-montserrat text-dark-gray font-bold md:text-[20rem] text-[10rem] leading-[0.7]'>4</p>
+            <img src="/images/wheel.png" alt="wheel" draggable={false} className='md:size-[16rem] size-[8rem]'/>
+            <p className='font-montserrat text-dark-gray font-bold md:text-[20rem] text-[10rem] leading-[0.7]'>4</p>
         </div>
 
-        <p className='mt-[3.5rem] font-montserrat font-bold text-[3.25rem] text-center text-dark-gray'>{ t('title') }</p>
-        <p className='text-dark-gray font-open-sans font-[400] text-[1.125rem] text-center'>{ t('desc') }</p>
-        <p className='text-dark-gray font-open-sans font-[400] text-[1.125rem] text-center'>{ t('msg') }... 00:00:{timer.toString().padStart(2, '0')}</p>
+        <p className='md:mt-[3.5rem] mt-[2rem] font-montserrat font-bold md:text-[3.25rem] text-[2rem] text-center text-dark-gray'>{ t('title') }</p>
+        <p className='text-dark-gray font-open-sans font-[400] text-[1.125rem] text-center mt-[1.5rem]'>{ t('desc') }</p>
+        <p className='text-dark-gray font-open-sans font-[400] text-[1.125rem] text-center mt-[1.5rem]'>{ t('msg') }... 00:00:{timer.toString().padStart(2, '0')}</p>
 
         <div className='flex justify-center mt-[2rem]'>
             <Link href="/">
