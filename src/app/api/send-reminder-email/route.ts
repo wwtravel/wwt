9ab4fb32decdde308418650add31e6a2918aaa08
Moving-Projects <info@trekmovers.com>
2024-09-09 17:39:03 +0300

    updates

--- src/app/api/send-reminder-email/route.ts
+++ src/app/api/send-reminder-email/route.ts
@@ -102,5 +102,19 @@ export async function GET () {
         }
     }
 
+    try {
+        await prisma.token.deleteMany({
+            where: {
+                expiresAt: {
+                    lt: new Date()
+                }
+            }
+        });
+    } catch (e) {
+        if (e) {
+            return handlePrismaError(e);
+        }
+    }
+
     return Response.json({msg: "Deleted expired dates!"}, {status: 200});
 }
